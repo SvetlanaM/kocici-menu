6d83efe338696479735b2335f191791b1fa3e075
SvetlanaM <richard.margeta@gmail.com>
2021-03-16 12:04:43 +0100

    Setup GraphQL, Apollo, Codegen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 
+import {
+  ApolloClient,
+  gql,
+  NormalizedCacheObject,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache: new InMemoryCache(),
+  headers: { 'x-hasura-admin-secret': 'catapp123' },
+  uri: 'https://cat-react-backend.herokuapp.com/v1/graphql',
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,40 @@
 import Head from 'next/head';
+import { useQuery, gql } from '@apollo/client';
+import { GetCatsQuery } from '../generated/graphql';
+
+const CATS_QUERY = gql`
+  query GetCats {
+    cats: cat {
+      id
+      name
+    }
+  }
+`;
+
+function CatList() {
+  const { loading, error, data } = useQuery<GetCatsQuery>(CATS_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  if (!data) return <p>No data</p>;
+
+  return (
+    <>
+      {data.cats.map((cat) => (
+        <div key={cat.id}>
+          <p>
+            {cat.name} : {cat.__typename}
+          </p>
+        </div>
+      ))}
+    </>
+  );
+}
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <CatList></CatList>
+    </div>
+  );
 }
