03b153cad5f909ba33eff452f5be30b8daaeab29
SvetlanaM <richard.margeta@gmail.com>
2021-05-04 19:52:03 +0200

    uprava mutation

--- netlify/functions/identity-signup/identity-signup.js
+++ netlify/functions/identity-signup/identity-signup.js
@@ -11,24 +11,84 @@
 // //   email: string;
 // // }
 
-const handler = async function (event) {
-  console.log('test');
-  const data = JSON.parse(event.body);
-  const { user } = data;
+// const handler = async function (event) {
+//   console.log('test');
+//   const data = JSON.parse(event.body);
+//   const { user } = data;
+
+//   const responseBody = {
+//     user_metadata: {
+//       ...user.user_metadata,
+//     },
+//   };
+
+//   const requestBodyString = JSON.stringify({
+//     query: `
+//     mutation InsertUser($email: String!, $id: uuid!) {
+//         insert_User_one(object:{id: $id, email: $email}) {
+//           id
+//         }
+//     }
+//   `,
+//     variables: {
+//       id: user.id,
+//       email: user.email,
+//     },
+//   });
+
+//   await axios({
+//     method: 'post',
+//     url: process.env.NEXT_PUBLIC_CAT_APP_TESTING_API_ENDPOINT || '',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'x-hasura-admin-secret': process.env.HASURA_PASSWORD,
+//     },
+//     body: requestBodyString,
+//   }).then(
+//     (response) => {
+//       console.log(response);
+//     },
+//     (error) => {
+//       console.log(error);
+//     }
+//   );
+
+//   console.log(requestBodyString);
+
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(responseBody),
+//   };
+// };
+
+// module.exports = { handler };
+
+const fetch = require('node-fetch');
+
+exports.handler = async function (event, context) {
+  const { user } = JSON.parse(event.body);
 
   const responseBody = {
+    app_metadata: {
+      roles:
+        user.email.split('@')[1] === 'trust-this-company.com'
+          ? ['editor']
+          : ['visitor'],
+      my_user_info: 'this is some user info',
+    },
     user_metadata: {
-      ...user.user_metadata,
+      ...user.user_metadata, // append current user metadata
+      custom_data_from_function: 'hurray this is some extra metadata',
     },
   };
 
-  const requestBodyString = JSON.stringify({
+  const responseBodyString = JSON.stringify({
     query: `
     mutation InsertUser($email: String!, $id: uuid!) {
         insert_User_one(object:{id: $id, email: $email}) {
           id
         }
-    }
+    }    
   `,
     variables: {
       id: user.id,
@@ -36,29 +96,31 @@ const handler = async function (event) {
     },
   });
 
-  await axios({
-    method: 'post',
-    url: process.env.NEXT_PUBLIC_CAT_APP_TESTING_API_ENDPOINT || '',
-    headers: {
-      'Content-Type': 'application/json',
-      'x-hasura-admin-secret': process.env.HASURA_PASSWORD,
-    },
-    body: requestBodyString,
-  }).then(
-    (response) => {
-      console.log(response);
-    },
-    (error) => {
-      console.log(error);
+  console.log(responseBodyString);
+
+  const result = await fetch(
+    process.env.NEXT_PUBLIC_CAT_APP_TESTING_API_ENDPOINT,
+    {
+      method: 'POST',
+      body: responseBodyString,
+      headers: {
+        'Content-Type': 'application/json',
+        'x-hasura-admin-secret': process.env.HASURA_SECRET,
+      },
     }
   );
+  const { errors, data } = await result.json();
 
-  console.log(requestBodyString);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(responseBody),
-  };
+  if (errors) {
+    console.log(errors);
+    return {
+      statusCode: 500,
+      body: 'Something is wrong',
+    };
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(responseBody),
+    };
+  }
 };
-
-module.exports = { handler };
