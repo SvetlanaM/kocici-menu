aaf435099089d832e424cb8d4e6cb8a4cb81854e
SvetlanaM <richard.margeta@gmail.com>
2021-05-04 21:09:07 +0200

    pokus o zmenu na axios

--- netlify/functions/identity-signup/identity-signup.js
+++ netlify/functions/identity-signup/identity-signup.js
@@ -1,7 +1,20 @@
-const fetch = require('node-fetch');
+// note - this function MUST be named `identity-signup` to work
+// we do not yet offer local emulation of this functionality in Netlify Dev
+//
+// more:
+// https://www.netlify.com/blog/2019/02/21/the-role-of-roles-and-how-to-set-them-in-netlify-identity/
+// https://www.netlify.com/docs/functions/#identity-and-functions
 
-exports.handler = async function (event, context) {
-  const { user } = JSON.parse(event.body);
+import axios from 'axios';
+
+// // interface UserType {
+// //   id: string;
+// //   email: string;
+// // }
+
+const handler = async function (event) {
+  const data = JSON.parse(event.body);
+  const { user } = data;
 
   const responseBody = {
     app_metadata: {
@@ -17,7 +30,7 @@ exports.handler = async function (event, context) {
     },
   };
 
-  const responseBodyString = JSON.stringify({
+  const requestBodyString = {
     query: `
     mutation InsertUser($email: String!, $id: uuid!) {
         insert_User_one(object:{id: $id, email: $email}) {
@@ -30,33 +43,34 @@ exports.handler = async function (event, context) {
       id: user.id,
       email: user.email,
     },
-  });
+  };
 
-  console.log(responseBodyString);
-
-  const result = await fetch(
-    process.env.NEXT_PUBLIC_CAT_APP_TESTING_API_ENDPOINT,
-    {
-      method: 'POST',
-      body: responseBodyString,
-      headers: {
-        'Content-Type': 'application/json',
-        'x-hasura-admin-secret': 'catapp123',
+  await axios
+    .post(
+      process.env.NEXT_PUBLIC_CAT_APP_TESTING_API_ENDPOINT,
+      requestBodyString,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'x-hasura-admin-secret': 'catapp123',
+        },
+      }
+    )
+    .then(
+      (response) => {
+        console.log(response);
       },
-    }
-  );
-  const { errors, data } = await result.json();
+      (error) => {
+        console.log(error);
+      }
+    );
 
-  if (errors) {
-    console.log(errors);
-    return {
-      statusCode: 500,
-      body: 'Something is wrong',
-    };
-  } else {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(responseBody),
-    };
-  }
+  console.log(requestBodyString);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(responseBody),
+  };
 };
+
+module.exports = { handler };
