f5c885517546aadd2a4be7df370b35182e535393
SvetlanaM <richard.margeta@gmail.com>
2021-06-30 09:49:36 +0200

    nove env

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ module.exports = {
     ignoreBuildErrors: true,
   },
   env: {
+    S3_UPLOAD_REGION: process.env.S3_UPLOAD_REGION,
+    S3_UPLOAD_KEY: process.env.S3_UPLOAD_KEY,
+    S3_UPLOAD_SECRET: process.env.S3_UPLOAD_SECRET,
     S3_UPLOAD_BUCKET: process.env.S3_UPLOAD_BUCKET,
   },
   // i18n,
