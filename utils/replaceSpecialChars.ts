eb208999ddfbc3110383f4f32088550e99300432
SvetlanaM <richard.margeta@gmail.com>
2021-07-27 12:58:36 +0200

    Update default values in select form logic

--- utils/replaceSpecialChars.ts
+++ utils/replaceSpecialChars.ts
@@ -1,8 +1,11 @@
 export const replaceSpecialChars = (str) => {
-  return str
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '') // Remove accents
-    .replace(/([^\w]+|\s+)/g, '-') // Replace space and other characters by hyphen
-    .replace(/\-\-+/g, '-') // Replaces multiple hyphens by one hyphen
-    .replace(/(^-+|-+$)/, ''); // Remove extra hyphens from beginning or end of the string
+  return (
+    str &&
+    str
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '') // Remove accents
+      .replace(/([^\w]+|\s+)/g, '-') // Replace space and other characters by hyphen
+      .replace(/\-\-+/g, '-') // Replaces multiple hyphens by one hyphen
+      .replace(/(^-+|-+$)/, '')
+  ); // Remove extra hyphens from beginning or end of the string
 };
