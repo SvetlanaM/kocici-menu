03924a7273776d7e2fa62bcf84bb3eb35f8281be
SvetlanaM <richard.margeta@gmail.com>
2021-08-11 14:56:53 +0200

    Remove console.log

--- utils/getUsername.ts
+++ utils/getUsername.ts
@@ -2,7 +2,6 @@ import setUppercaseTitle from './setUppercaseTitle';
 
 export const getUsername = (email: string): string => {
   let name = setUppercaseTitle(email.substr(0, email.lastIndexOf('@')));
-  console.log(name.substr(0, name.lastIndexOf('.')));
   if (name.substr(0, name.lastIndexOf('.')) !== '') {
     name = name.substr(0, name.lastIndexOf('.'));
     return name;
