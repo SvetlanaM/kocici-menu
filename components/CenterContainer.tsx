ddaee0151669c587676a93b8c58f5eceaa4f5bb5
SvetlanaM <richard.margeta@gmail.com>
2021-09-19 22:48:02 +0200

    CR CenterContainer

--- components/CenterContainer.tsx
+++ components/CenterContainer.tsx
@@ -2,7 +2,7 @@ type ContainerProps = {
   children: React.ReactNode;
 };
 
-const CenterContainer = ({ children }: ContainerProps) => {
+const CenterContainer = ({ children }: ContainerProps): JSX.Element => {
   return (
     <div className="w-full xl-custom:w-9/12 mt-16 xl-custom:mt-0">
       {children}
--- components/Container.tsx
+++ components/Container.tsx
@@ -2,7 +2,7 @@ type ContainerProps = {
   children: React.ReactNode;
 };
 
-const Container = ({ children }: ContainerProps) => {
+const Container = ({ children }: ContainerProps): JSX.Element => {
   return (
     <div className="w-full h-screen flex overflow-y-hidden">
       <div className="w-full flex-col-base pt-5 xl-custom:pt-9.5 p-8 xl-custom:p-8 overflow-x-hidden">
