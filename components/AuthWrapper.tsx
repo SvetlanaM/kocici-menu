790fde39e5b358b97fa8151e9c56aefbc00a2e48
SvetlanaM <richard.margeta@gmail.com>
2021-09-19 17:13:33 +0200

    CR AuthWrapper

--- components/AuthWrapper.tsx
+++ components/AuthWrapper.tsx
@@ -2,7 +2,7 @@ type AuthWrapperProps = {
   children: React.ReactNode;
 };
 
-const AuthWrapper = ({ children }: AuthWrapperProps) => {
+const AuthWrapper = ({ children }: AuthWrapperProps): JSX.Element => {
   return (
     <div className="flex flex-col xl-custom:flex-row w-full justify-items-stretch">
       {children}
