759cdcbb2d7151d971b4b3893b1dbba0aa205f1d
SvetlanaM <richard.margeta@gmail.com>
2021-09-19 17:09:59 +0200

    CR AuthLeftContainer

--- components/AuthLeftContainer.tsx
+++ components/AuthLeftContainer.tsx
@@ -3,7 +3,10 @@ type AuthLeftContainerProps = {
   className: string;
 };
 
-const AuthLeftContainer = ({ children, className }: AuthLeftContainerProps) => {
+const AuthLeftContainer = ({
+  children,
+  className,
+}: AuthLeftContainerProps): JSX.Element => {
   return (
     <div
       className={`order-1 xl-custom:order-2 w-full xl-custom:w-1/2 bg-purple-darkest ${className}`}
