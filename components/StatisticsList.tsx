6b293da1041da3826d305fb8912f5f8467121866
SvetlanaM <richard.margeta@gmail.com>
2021-09-20 11:06:12 +0200

    CR StatisticsList

--- components/StatisticsList.tsx
+++ components/StatisticsList.tsx
@@ -1,21 +1,22 @@
 import StatisticBox from './StatisticBox';
 import InnerContainer from './InnerContainer';
+import { GetDashboardQuery } from '../graphql/generated/graphql';
 
 interface StatisticsListProps {
-  data: Array<any>;
+  data: GetDashboardQuery['fav_stats'] | GetDashboardQuery['stats'];
   cols: string;
 }
 
-const StatisticsList = ({ data, cols }: StatisticsListProps) => {
+const StatisticsList = ({ data, cols }: StatisticsListProps): JSX.Element => {
   return (
     <InnerContainer flexType="flew-col">
       <div
         className={`grid grid-cols xl-custom:${cols} grid-flow-row gap-y-11 xl-custom:gap-x-11 w-full`}
       >
         {data &&
-          data.map((item, id) => (
+          data.map((item) => (
             <StatisticBox
-              key={item.name + id}
+              key={item.name + item.title}
               icon={item.icon}
               name={item.name}
               title={item.title}
