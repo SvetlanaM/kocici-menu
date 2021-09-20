55294d4084ab5afebe7f6dd606ad716d68e19567
SvetlanaM <richard.margeta@gmail.com>
2021-09-20 09:59:52 +0200

    CR FormInputLabel

--- components/FormInputLabel.tsx
+++ components/FormInputLabel.tsx
@@ -1,4 +1,8 @@
-const FormInputLabel = ({ name }) => (
+interface FormInputLabelProps {
+  name: string;
+}
+
+const FormInputLabel = ({ name }: FormInputLabelProps): JSX.Element => (
   <label className="mb-1">
     <span className="base-medium-text font-light text-purple mb-1">{name}</span>
   </label>
