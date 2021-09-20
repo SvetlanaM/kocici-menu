9f5a2f3398659bc4e4c21ccc0b27da9788ee2f98
SvetlanaM <richard.margeta@gmail.com>
2021-09-20 09:58:52 +0200

    CR FormInput

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,23 +1,21 @@
 import { forwardRef } from 'react';
+import { FieldError } from 'react-hook-form';
 
 interface FormInputProps {
-  registerRules?: any;
-  errors?: any;
+  errors?: FieldError;
   type: string;
   placeholder?: string;
-  defaultValue?: any;
+  defaultValue?: string;
   width?: string;
   step?: number;
   onChange?: (e) => void;
   name?: string;
   required?: boolean;
-  rest?: any;
 }
 
-const FormInput = forwardRef(
+const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
   (
     {
-      registerRules,
       errors,
       type,
       placeholder,
@@ -26,15 +24,12 @@ const FormInput = forwardRef(
       name,
       step,
       onChange,
-      required,
-      ...rest
     }: FormInputProps,
     ref
-  ) => {
+  ): JSX.Element => {
     return (
       <input
         ref={ref}
-        // {...registerRules}
         className={`form-input mb-3 mt-2 text-purple block border-rounded-base ${width} ${
           errors ? 'border-red-400' : 'border-gray'
         }
@@ -42,48 +37,14 @@ const FormInput = forwardRef(
               focus:border focus:ring-gray focus:ring-opacity-50 placeholder-gray`}
         type={type}
         placeholder={placeholder}
-        // defaultValue={defaultValue}
         onChange={onChange}
         value={defaultValue}
         step={step}
         name={name}
         autoComplete="on"
-        {...rest}
       />
     );
   }
 );
-
-// const FormInput = ({
-//   registerRules,
-//   errors,
-//   type,
-//   placeholder,
-//   defaultValue,
-//   width,
-//   name,
-//   step,
-//   onChange,
-// }: FormInputProps) => {
-//   return (
-//     <input
-//       ref={registerRules}
-//       // {...registerRules}
-//       className={`form-input mb-3 mt-2 text-purple block border-rounded-base ${width} ${
-//         errors ? 'border-red-400' : 'border-gray'
-//       }
-//               focus:outline-none focus:bg-white focus:border-gray
-//               focus:border focus:ring-gray focus:ring-opacity-50 placeholder-gray`}
-//       type={type}
-//       placeholder={placeholder}
-//       defaultValue={defaultValue}
-//       onChange={onChange}
-//       value={defaultValue}
-//       step={step}
-//       name={name}
-//       autoComplete="on"
-//     />
-//   );
-// };
 
 export default FormInput;
