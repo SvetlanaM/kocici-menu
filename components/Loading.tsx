9fa8c3be0da8431bdd42c18f88b2c7290f943c41
SvetlanaM <richard.margeta@gmail.com>
2021-09-20 10:20:29 +0200

    CR Loading

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,4 +1,4 @@
-export default function Loading() {
+export default function Loading(): JSX.Element {
   return (
     <>
       <div className="flex h-screen justify-center items-center z-10">
@@ -9,11 +9,15 @@ export default function Loading() {
             cy="12"
             r="10"
             strokeWidth="4"
-          ></circle>
+          >
+            {''}
+          </circle>
           <path
             className="fill-current text-purple"
             d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          ></path>
+          >
+            {''}
+          </path>
         </svg>
       </div>
     </>
