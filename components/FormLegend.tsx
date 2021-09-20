158b9dcb6a5e630601fcd4fff624e22ce5c52362
SvetlanaM <richard.margeta@gmail.com>
2021-09-20 10:03:02 +0200

    CR FormLegend

--- components/FormLegend.tsx
+++ components/FormLegend.tsx
@@ -1,4 +1,8 @@
-const FormLegend = ({ name }) => {
+interface FormLegendProps {
+  name: string;
+}
+
+const FormLegend = ({ name }: FormLegendProps): JSX.Element => {
   return <legend className="pb-4 font-semibold text-purple">{name}</legend>;
 };
 
