4b920fc5d524af71119b0c08c284363f06c1746d
SvetlanaM <richard.margeta@gmail.com>
2021-09-20 10:18:05 +0200

    CR LeftContainer

--- components/LeftContainer.tsx
+++ components/LeftContainer.tsx
@@ -1,8 +1,8 @@
-type ContainerProps = {
+interface ContainerProps {
   children: React.ReactNode;
-};
+}
 
-const LeftContainer = ({ children }: ContainerProps) => {
+const LeftContainer = ({ children }: ContainerProps): JSX.Element => {
   return (
     <div className="w-full pt-5 xl-custom:pt-0 xl-custom:w-3/12 xl-custom:pl-7">
       {children}
