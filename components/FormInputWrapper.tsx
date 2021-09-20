a3f6989d0ff5dd3bd14d1af9a505a6b5d91a1980
SvetlanaM <richard.margeta@gmail.com>
2021-09-20 10:00:37 +0200

    CR FormInputWrapper

--- components/FormInputWrapper.tsx
+++ components/FormInputWrapper.tsx
@@ -1,4 +1,8 @@
-const FormInputWrapper = ({ children }) => (
+interface FormInputWrapperProps {
+  children: React.ReactNode;
+}
+
+const FormInputWrapper = ({ children }: FormInputWrapperProps): JSX.Element => (
   <div className="flex flex-col w-full mb-1"> {children}</div>
 );
 
