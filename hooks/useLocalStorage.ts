bd568225b975fb509845253dc88c533dd8fc1542
SvetlanaM <richard.margeta@gmail.com>
2021-09-20 11:46:23 +0200

    CR useLocalStorage

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,33 +1,34 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export enum LocalStorageKey {
-    SELECTED_CAT = 'catDetail.selectedCat'
+  SELECTED_CAT = 'catDetail.selectedCat',
 }
 
-const useLocalStorage = (key: LocalStorageKey, initialValue) => {
+const useLocalStorage = (
+  key: LocalStorageKey,
+  initialValue: string
+): Array<string> => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      return initialValue;
+    }
+  });
 
-    const [storedValue, setStoredValue] = useState(() => {
-        try {
-            const item = window.localStorage.getItem(key);
-            return item ? JSON.parse(item) : initialValue;
-        } catch (error) {
-            console.log(error);
-            return initialValue;
-        }
-    });
+  const setValue = (value) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const setValue = value => {
-        try {
-            const valueToStore =
-                value instanceof Function ? value(storedValue) : value;
-            setStoredValue(valueToStore);
-            window.localStorage.setItem(key, JSON.stringify(valueToStore));
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  return [storedValue, setValue];
+};
 
-    return [storedValue, setValue];
-}
-
-export default useLocalStorage
+export default useLocalStorage;
