84a95de6f4b015bee7016860aa2abe053cb438eb
Ondra Mařík <ondra.marik@gmail.com>
2021-11-03 15:36:00 +0100

    Fix show password icon on safari

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -68,8 +68,7 @@ const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
           />
         )}
         {passClass && (
-          <img
-            src=""
+          <div
             className={`${passClass} absolute pass`}
             onClick={onClick}
           />
