00ce075f88b18f94df2b00d1d5ddcfb429ab9b05
SvetlanaM <richard.margeta@gmail.com>
2021-11-09 14:55:26 +0100

    Modal overflow

--- components/Containers/Container.tsx
+++ components/Containers/Container.tsx
@@ -4,7 +4,7 @@ type ContainerProps = {
 
 const Container = ({ children }: ContainerProps): JSX.Element => {
   return (
-    <div className="w-full h-auto xl-custom:max-h-screen flex overflow-y-hidden">
+    <div className="w-full h-auto xl-custom:max-h-screen flex xl-custom:overflow-y-hidden">
       <div className="w-full flex-col-base pt-20 xl-custom:pt-9.5 p-8 xl-custom:p-8 overflow-x-hidden">
         <div className="flex flex-wrap">{children}</div>
       </div>
